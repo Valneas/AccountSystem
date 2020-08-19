a4ca7d54e1b79a21a00723b816626004fc57a9d8
BuildTools <unconfigured@null.spigotmc.org>
2020-08-19 14:49:32 +0200

    Added comments, command 'account' support and some events registeration.

--- src/main/java/net/valneas/account/AccountSystem.java
+++ src/main/java/net/valneas/account/AccountSystem.java
@@ -1,11 +1,9 @@
 package net.valneas.account;
 
+import net.valneas.account.api.commands.AccountCommand;
 import net.valneas.account.api.commands.PermissionCommand;
 import net.valneas.account.api.commands.RankCommand;
-import net.valneas.account.events.PermissionAssignedListener;
-import net.valneas.account.events.PermissionUnAssignedListener;
-import net.valneas.account.events.PlayerJoinListener;
-import net.valneas.account.events.PlayerQuitListener;
+import net.valneas.account.listener.*;
 import net.valneas.account.mongo.Mongo;
 import net.valneas.account.rank.permission.PermissionManager;
 import net.valneas.account.util.MongoUtil;
@@ -18,38 +16,65 @@ public class AccountSystem extends JavaPlugin {
     private final MongoUtil mongoUtil = new MongoUtil(this);
     private final PermissionManager permissionManager = new PermissionManager(mongo.getMongoClient());
 
+    /**
+     * Plugin initialization
+     */
     @Override
     public void onEnable() {
         saveDefaultConfig();
+
         registerEvents();
         getCommand("permission").setExecutor(new PermissionCommand());
         getCommand("rank").setExecutor(new RankCommand(this));
+        getCommand("account").setExecutor(new AccountCommand(this));
         getLogger().info("Enabled!");
     }
 
+    /**
+     * Registering events
+     */
     private void registerEvents() {
         registerListeners(
                 new PlayerJoinListener(this),
                 new PlayerQuitListener(this),
                 new PermissionAssignedListener(),
-                new PermissionUnAssignedListener()
+                new PermissionUnAssignedListener(),
+                new MajorRankChangedListener(this),
+                new RankAddedListener(this),
+                new RankRemovedListener(this)
         );
     }
 
+    /**
+     * Register a listener
+     * @param listeners : Listener to register
+     */
     private void registerListeners(Listener...listeners){
         for (Listener listener : listeners) {
             getServer().getPluginManager().registerEvents(listener, this);
         }
     }
 
+    /**
+     * Get a mongo class instance
+     * @return mongo class instance
+     */
     public Mongo getMongo() {
         return mongo;
     }
 
+    /**
+     * Get a mongo util class instance
+     * @return mongo util class instance
+     */
     public MongoUtil getMongoUtil() {
         return mongoUtil;
     }
 
+    /**
+     * Get a permission manager class instance
+     * @return permission manager class instance
+     */
     public PermissionManager getPermissionManager() {
         return permissionManager;
     }
