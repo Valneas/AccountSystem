7eac6f18a797e2376f6be5d0e6e19519fce44354
Azodox_ <likemoi99@gmail.com>
2020-08-30 20:41:01 +0200

    Removed all @

--- src/main/java/net/valneas/account/rank/RankUnit.java
+++ src/main/java/net/valneas/account/rank/RankUnit.java
@@ -8,20 +8,20 @@ import java.util.List;
 
 public enum RankUnit {
 
-    ADMIN(1, 1, "Admin", "§4@Admin ", "", ChatColor.DARK_RED),
-    HEAD(2, 2, "Head", "@Head-", "", ChatColor.BOLD),
-    DEVELOPPEUR(3, 3, "Développeur", "§c§o@Développeur ", "", ChatColor.RED),
-    DEVELOPPEUSE(4, 4, "Développeuse", "§c§o@Développeuse ", "", ChatColor.RED),
-    BUILDER(5, 5, "Builder", "§b@Builder ", "", ChatColor.AQUA),
-    BUILDEUSE(6, 6, "Buildeuse", "§b@Buildeuse ", "", ChatColor.AQUA),
-    MODERATEUR(7, 7, "Modérateur", "§2@Modérateur ", "", ChatColor.DARK_GREEN),
-    MODERATRICE(8, 8, "Modératrice", "§2@Modératrice ", "", ChatColor.DARK_GREEN),
-    ASSISTANT(9, 9, "Assistant", "§a@Assistant ", "", ChatColor.GREEN),
-    ASSISTANTE(10, 10, "Assistante", "§a@Assistante ", "", ChatColor.GREEN),
+    ADMIN(1, 1, "Admin", "§4Admin ", "", ChatColor.DARK_RED),
+    HEAD(2, 2, "Head", "Head-", "", ChatColor.BOLD),
+    DEVELOPPEUR(3, 3, "Développeur", "§c§oDéveloppeur ", "", ChatColor.RED),
+    DEVELOPPEUSE(4, 4, "Développeuse", "§c§oDéveloppeuse ", "", ChatColor.RED),
+    BUILDER(5, 5, "Builder", "§bBuilder ", "", ChatColor.AQUA),
+    BUILDEUSE(6, 6, "Buildeuse", "§bBuildeuse ", "", ChatColor.AQUA),
+    MODERATEUR(7, 7, "Modérateur", "§2Modérateur ", "", ChatColor.DARK_GREEN),
+    MODERATRICE(8, 8, "Modératrice", "§2Modératrice ", "", ChatColor.DARK_GREEN),
+    ASSISTANT(9, 9, "Assistant", "§aAssistant ", "", ChatColor.GREEN),
+    ASSISTANTE(10, 10, "Assistante", "§aAssistante ", "", ChatColor.GREEN),
     STAFF(11, 15, "Staff", "§d§oStaff ", "", ChatColor.LIGHT_PURPLE),
-    PARTENAIRE(12, 11, "Partenaire", "§6@Partenaire ", "", ChatColor.GOLD),
-    YOUTUBE(13, 12, "YouTube", "§c§n@YouTube ", "", ChatColor.RED),
-    TWITCH(14, 13, "Twitch", "§5§n@Twitch ", "", ChatColor.DARK_PURPLE),
+    PARTENAIRE(12, 11, "Partenaire", "§6Partenaire ", "", ChatColor.GOLD),
+    YOUTUBE(13, 12, "YouTube", "§c§nYouTube ", "", ChatColor.RED),
+    TWITCH(14, 13, "Twitch", "§5§nTwitch ", "", ChatColor.DARK_PURPLE),
     JOUEUR(15, 14, "Joueur", "§7", "", ChatColor.GRAY);
 
     private final String name, prefix, suffix;
